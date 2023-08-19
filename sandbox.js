3d185c76f888c9b21b9884b97913b11aa26b28e7
sytnikov <sytnikov.alexey@gmail.com>
2023-08-19 21:09:42 +0300

    compilation of ts files

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+var character = 'princess';
+console.log(character);
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(30));
+// arrays
+var names = ['mario', 'luigi', 'yoshi'];
+names.push('li');
+var mixed = [3, 5, 'string', false];
+mixed.push(true);
+// objects
---
+++ sandbox.ts
@@ -0,0 +1,21 @@
+const character = 'princess';
+
+console.log(character);
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI
+}
+
+console.log(circ(30));
+
+// arrays
+let names = ['mario', 'luigi', 'yoshi'];
+
+names.push('li');
+
+let mixed = [3, 5, 'string', false];
+
+mixed.push(true);
+
+// objects
+
